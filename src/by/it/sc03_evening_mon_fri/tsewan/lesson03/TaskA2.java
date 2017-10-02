a331af139e04753cee67a2ced3a817d475246f7a
TsewanDiana <tsevad@mail.ru>
2017-10-02 21:25:00 +0300

    task 1,2

---
+++ src/by/it/sc03_evening_mon_fri/tsewan/lesson03/TaskA2.java
@@ -0,0 +1,9 @@
+package by.it.sc03_evening_mon_fri.tsewan.lesson03;
+    class TaskA2 {
+    public static void main(String[] args) {
+        for (int i = 0; i< 5; i++)
+        {
+            System.out.println("Я начинаю учить Java!");
+        }
+    }
+}
