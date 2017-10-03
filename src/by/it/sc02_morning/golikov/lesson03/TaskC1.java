6d7886bce5cc72567b78fe55f5d4dfffe9330a8b
vgolikov <vvgolikov7@gmail.com>
2017-10-03 17:53:55 +0300

    A1, A2, A3, B1, B2, B3, C1 (fail, but the program counts and shows a true result as in the example: Sum = "number") complete

--- src/by/it/sc02_morning/golikov/lesson03/TaskC1.java
+++ src/by/it/sc02_morning/golikov/lesson03/TaskC1.java
@@ -9,8 +9,16 @@ package by.it.sc02_morning.golikov.lesson03;
 Вывод:
 Sum = 26
 */
+import java.util.Scanner;
 class TaskC1 {
-
+    public static void main(String[] args) {
+        Scanner sc1 = new Scanner(System.in);
+        Scanner sc2 = new Scanner(System.in);
+        int i = sc1.nextInt();
+        int j = sc2.nextInt();
+        int result = i+j;
+        System.out.println("Sum" + " " + "=" + " " + result);
+    }
 
 
 }
