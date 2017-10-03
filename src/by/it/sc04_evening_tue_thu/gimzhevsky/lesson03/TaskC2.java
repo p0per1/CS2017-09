0aa92bcf2f149b36283da2a2b6ddfe3ef3275ed7
gimzhevsky <gimvit@gmail.com>
2017-10-03 21:23:12 +0300

    Lesson 3, Task A completed

---
+++ src/by/it/sc04_evening_tue_thu/gimzhevsky/lesson03/TaskC2.java
@@ -0,0 +1,26 @@
+package by.it.sc04_evening_tue_thu.gimzhevsky.lesson03;
+
+import java.util.Scanner;
+
+/* Нужно написать программу, которая вводит два числа с клавиатуры
+и 4 раза выводит их сумму с обозначением системы счисления на экран в
+ДЕСЯТИЧНОМ ДВОИЧНОМ ШЕСТНАДЦАТИРИЧНОМ ВОСЬМИРИЧНОМ виде
+
+Ввод:
+34 26
+
+Вывод:
+DEC:34+26=60
+BIN:100010+11010=111100
+HEX:22+1a=3c
+OKT:42+32=74
+*/
+class TaskC2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);;
+        int i = sc.nextInt();
+        int j = sc.nextInt();
+      //s  System.out.println("Sum = " + sum);
+    }
+
+}
