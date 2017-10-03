13c4c3384a74132fea9b35e44c5cec6ee7ba9a72
vgolikov <vvgolikov7@gmail.com>
2017-10-03 16:55:02 +0300

    A1, A2, A3, B1, B2, B3 complete

--- src/by/it/sc02_morning/golikov/lesson03/TaskB2.java
+++ src/by/it/sc02_morning/golikov/lesson03/TaskB2.java
@@ -15,9 +15,9 @@ package by.it.sc02_morning.golikov.lesson03;
 
  */
 class TaskB2 {
-    private static int a = 1;
-    private static int b = 3;
-    private static int c = 9;
+    private static int a = -1;
+    private static int b = +3;
+    private static int c = -9;
     private static int d = 27;
 
     public static void main(String[] args) {
