25b10213d3e3165cb78d6f5e3f732867f61eecda
p0per1 <agoodworker@yandex.ru>
2017-10-04 15:06:56 +0300

    Tack C1 (0.1)

--- src/by/it/sc04_evening_tue_thu/popruga/lesson03/TaskC1.java
+++ src/by/it/sc04_evening_tue_thu/popruga/lesson03/TaskC1.java
@@ -8,9 +8,16 @@ package by.it.sc04_evening_tue_thu.popruga.lesson03;
 
 Вывод:
 Sum = 26
-*/
+ */
+
+import java.util.Scanner;
+
 class TaskC1 {
-
-
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        int j = sc.nextInt();
+        int result=i + j;
+        System.out.println(result);
+    }
 }
