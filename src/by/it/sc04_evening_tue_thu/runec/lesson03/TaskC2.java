3166d2a32936cf947fb141106c2c3d92a851eea8
Victoria <travel.vi@mail.ru>
2017-10-04 22:12:20 +0300

    Complete Task C2

--- src/by/it/sc04_evening_tue_thu/runec/lesson03/TaskC2.java
+++ src/by/it/sc04_evening_tue_thu/runec/lesson03/TaskC2.java
@@ -13,7 +13,18 @@ BIN:100010+11010=111100
 HEX:22+1a=3c
 OKT:42+32=74
 */
+import java.util.Scanner;
 class TaskC2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int sum = a + b;
+        System.out.println("DEC:" + a + "+" + b + "=" + sum);
+        System.out.println("BIN:"+Integer.toBinaryString(a)+"+"+Integer.toBinaryString(b)+"="+Integer.toBinaryString(sum));
+        System.out.println("HEX:"+Integer.toHexString(a)+"+"+Integer.toHexString(b)+"="+Integer.toHexString(sum));
+        System.out.println("OKT:"+Integer.toOctalString(a)+"+"+Integer.toOctalString(b)+"="+Integer.toOctalString(sum));
+    }
+    }
 
 
-}
