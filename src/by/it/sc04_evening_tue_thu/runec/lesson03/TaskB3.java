964ad9bb87369377f720258c58da1013757352ea
Victoria <travel.vi@mail.ru>
2017-10-04 21:44:26 +0300

    Complete Task C1

--- src/by/it/sc04_evening_tue_thu/runec/lesson03/TaskB3.java
+++ src/by/it/sc04_evening_tue_thu/runec/lesson03/TaskB3.java
@@ -11,14 +11,14 @@ package by.it.sc04_evening_tue_thu.runec.lesson03;
 class TaskB3 {
         public static void main(String[] args) {
 //        String s = "C";
-//        System.out.println("Новым Годом");
-//        System.out.println("C Новым");
+//       System.out.println("Новым Годом");
+ //     System.out.println("C Новым");
 //        System.out.println("Годом");
-//        System.out.print("C Новым");
+        System.out.print("C Новым");
 //        System.out.println("Годом");
-//        System.out.println(s);
+//       System.out.println(s);
 //        System.out.print("C Новым!");
-//        System.out.print(" ");
-//        System.out.println("Годом");
+        System.out.print(" ");
+        System.out.println("Годом");
     }
 }
--- src/by/it/sc04_evening_tue_thu/runec/lesson03/TaskC1.java
+++ src/by/it/sc04_evening_tue_thu/runec/lesson03/TaskC1.java
@@ -9,8 +9,19 @@ package by.it.sc04_evening_tue_thu.runec.lesson03;
 Вывод:
 Sum = 26
 */
+
+import java.util.Scanner;
+
 class TaskC1 {
 
 
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+    }
 }
