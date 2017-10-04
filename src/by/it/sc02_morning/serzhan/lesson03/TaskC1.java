87517eb77d7a9599a77dbe105b083703c398d4a0
Elena <serzhan.helen@gmail.com>
2017-10-04 20:20:06 +0300

    Task C1 complete

--- src/by/it/sc02_morning/serzhan/lesson03/TaskC1.java
+++ src/by/it/sc02_morning/serzhan/lesson03/TaskC1.java
@@ -15,22 +15,12 @@ Sum = 26
 class TaskC1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s1, s2;
-        s1 = sc.nextLine();
-        s2 = sc.nextLine();
-        int result=s1+s2;
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int result=b+a;
         String sum;
-        sum="Sum="+result;
+        sum="Sum = "+result;
         System.out.println(sum);
-      //  Scanner sc = new Scanner(System.in);
-      //  int i = sc.next();
-        //int a=sc.next();
-        //int result=i+a;
-        //String sum;
-        //sum="Sum="+result;
-        //System.out.println(sum);
+
     }
-
-
-
 }
