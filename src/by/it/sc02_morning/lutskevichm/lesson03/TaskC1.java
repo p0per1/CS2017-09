25885230d5bca293d8d2598daaf0ad9d8ae5d6cb
Mary <lutskevichmaria@gmail.ru>
2017-10-04 22:03:57 +0300

    Task A3, B2, B3 complete. Task C1, C2, C3 with right result, but failed the test

--- src/by/it/sc02_morning/lutskevichm/lesson03/TaskC1.java
+++ src/by/it/sc02_morning/lutskevichm/lesson03/TaskC1.java
@@ -1,5 +1,7 @@
 package by.it.sc02_morning.lutskevichm.lesson03;
 
+import java.util.Scanner;
+
 /* Нужно написать программу, которая вводит два числа с клавиатуры
 и выводит их сумму на экран в виде
 
@@ -10,6 +12,18 @@ package by.it.sc02_morning.lutskevichm.lesson03;
 Sum = 26
 */
 class TaskC1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int i=sc.nextInt();
+        Scanner scj=new Scanner(System.in);
+        int j=scj.nextInt();
+        int res=i+j;
+        System.out.println("Ввод:");
+        System.out.println(i + " " + j);
+        System.out.println();
+        System.out.println("Вывод:");
+        System.out.println("Sum = " + res);
+    }
 
 
 
