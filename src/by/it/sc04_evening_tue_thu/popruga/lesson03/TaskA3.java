663d6bd42b578fdec90f52e7376429d6dfb5b0e1
p0per1 <agoodworker@yandex.ru>
2017-10-04 13:48:24 +0300

    Tack A3 and B1

--- src/by/it/sc04_evening_tue_thu/popruga/lesson03/TaskA3.java
+++ src/by/it/sc04_evening_tue_thu/popruga/lesson03/TaskA3.java
@@ -1,4 +1,4 @@
-package by.it.sc04_evening_tue_thu.popruga;
+package by.it.sc04_evening_tue_thu.popruga.lesson03;
 /*
 Измените переменные и строку вывода в программе так,
 чтобы она расчитывала и печатала следующее выражение
@@ -7,9 +7,10 @@ package by.it.sc04_evening_tue_thu.popruga;
 
 class TaskA3 {
     public static void main(String[] args) {
-        int i = 2;
-        int j = 3;
-        int k = i + j;
-        System.out.println(i + "+" + j + "=" + k);
+        int i = 3;
+        int j = 4;
+        int k = i*i+j*j;
+        System.out.println(i+"*"+i+"+"+j+"*"+j+"="+k);
+
     }
 }
