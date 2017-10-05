a51293719a174d83f995544cda0ffa588dfe454f
Stepan <s.menchytsky@gmail.com>
2017-10-05 11:55:22 +0300

    готово

---
+++ src/by/it/sc02_morning/menchytsky/lesson03/TaskC2.java
@@ -0,0 +1,31 @@
+package by.it.sc02_morning.menchytsky.lesson03;
+
+import java.util.Scanner;
+
+/* Нужно написать программу, которая вводит два числа с клавиатуры
+и 4 раза выводит их сумму с обозначением системы счисления на экран в
+ДЕСЯТИЧНОМ ДВОИЧНОМ ШЕСТНАДЦАТИРИЧНОМ ВОСЬМИРИЧНОМ виде
+
+Ввод:
+34 26
+
+Вывод:
+DEC:34+26=60
+BIN:100010+11010=111100
+HEX:22+1a=3c
+OKT:42+32=74
+*/
+class TaskC2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println("DEC:"+a+"+"+b+"="+sum);
+        System.out.println("BIN:"+Integer.toBinaryString(a)+"+"+Integer.toBinaryString(b)+"="+Integer.toBinaryString(sum));
+        System.out.println("HEX:"+Integer.toHexString(a)+"+"+Integer.toHexString(b)+"="+Integer.toHexString(sum));
+        System.out.println("OKT:"+Integer.toOctalString(a)+"+"+Integer.toOctalString(b)+"="+Integer.toOctalString(sum));
+
+    }
+
+}
